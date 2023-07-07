95e36c6a4cbec5f860610583b545524bbd3d95ab
Deepak Suthar <codewithdeepak@yahoo.com>
2023-07-07 14:38:30 +0530

    fix: writing whole file

--- hello.js
+++ hello.js
@@ -1,1 +1,7 @@
-import Table from "@mui/material/Table";
+import Table from "@mui/material/Table"
+import Box from "@mui/material/Box"
+import Link from "@mui/material/Link"
+
+function hello() {
+    console.log("Hello")
+}
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::fs;
 use std::io::{self, BufRead};
 use std::path::Path;
 use regex::Regex;
+use std::io::Write;
 
 fn main() -> io::Result<()> {
     // Read the JavaScript file
@@ -14,8 +15,13 @@ fn main() -> io::Result<()> {
     // Regular expression to match import statements
     let import_regex = Regex::new(r#"import \{(\w+)\} from "@mui/material""#).unwrap();
 
+    let temp_file_path = Path::new("hello_tmp.js");
+    let mut temp_file = File::create(temp_file_path)?;
+
+    let lines = reader.lines();
+    
     // Iterate over each line in the file
-    for line in reader.lines() {
+    for line in lines {
         let line = line?;
 
         if let Some(captures) = import_regex.captures(&line) {
@@ -24,14 +30,19 @@ fn main() -> io::Result<()> {
 
             let dynamic_import_string = format!("import {} from \"@mui/material/{}\"",imported_module,imported_module);
         
-            let replaced_content = import_regex.replace_all(&line, dynamic_import_string);
+            let replaced_content = import_regex.replace(&line, dynamic_import_string.as_str());
 
-            fs::write("hello.js", replaced_content.to_string()).expect("Failed to write file");
+            writeln!(temp_file, "{}", replaced_content)?;
 
             println!("Imported module: {}", imported_module);
+        }else{
+            println!("Running {}",line);
+            writeln!(temp_file, "{}", line)?;
         }
-
     }
+
+    fs::rename("hello_tmp.js","hello.js")?;
+
 
     Ok(())
 }
